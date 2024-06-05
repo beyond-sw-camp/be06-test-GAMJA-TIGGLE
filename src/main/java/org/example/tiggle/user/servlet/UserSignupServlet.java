e35aa568dd160a985f67c134d1255c93fa260f7d
yeon <dutls5367@gmail.com>
2024-06-05 15:02:24 +0900

    [Feat] UserSignupServlet

--- src/main/java/org/example/tiggle/user/servlet/UserSignupServlet.java
+++ src/main/java/org/example/tiggle/user/servlet/UserSignupServlet.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.tiggle.config.BaseResponse;
 import org.example.tiggle.config.BaseResponseMessage;
-import org.example.tiggle.user.DAO.MemberDao;
-import org.example.tiggle.user.DTO.request.PostMemberReq;
+import org.example.tiggle.user.DAO.UserDao;
+import org.example.tiggle.user.DTO.request.PostUserSignupReq;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,20 +15,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-@WebServlet("/member/signup")
-public class MemberSignupServlet extends HttpServlet {
-    MemberDao dao;
+@WebServlet("/user/signup")
+public class UserSignupServlet extends HttpServlet {
+    UserDao dao;
     ObjectMapper mapper;
 
     @Override
     public void init() {
-        dao = new MemberDao();
+        dao = new UserDao();
         mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         // ------------------- 클라이언트로부터 요청을 받아서 Dto에 저장하는 부분 -------------------
         // 역직렬화 | JSON -> 객체
@@ -39,9 +39,11 @@ public class MemberSignupServlet extends HttpServlet {
             json.append(line);
         }
 
-        PostMemberReq dto = mapper.readValue(json.toString(), PostMemberReq.class);
-        // ------------------- ------------------- -------------------
 
+        PostUserSignupReq dto = mapper.readValue(json.toString(), PostUserSignupReq.class);
+        // ------------------- ------------------- -------------------
+        // dto에 값을 잘 전달 했는지 보기 위해서 써봄
+        // System.out.println(dto.toString());
 
         // ------------------- 회원 가입하는 Dao의 메소드 실행 -------------------
         Boolean result = dao.create(dto);
