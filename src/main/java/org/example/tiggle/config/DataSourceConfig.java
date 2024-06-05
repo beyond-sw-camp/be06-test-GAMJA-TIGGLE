4339b105e733a20df074595801adecf51440c7d2
postrel63 <postrel63@naver.com>
2024-06-05 10:07:37 +0900

    git init 초기 패키지 설정 user, program, reservation으로 구분

---
+++ src/main/java/org/example/tiggle/config/DataSourceConfig.java
@@ -0,0 +1,25 @@
+package org.example.tiggle.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class DataSourceConfig {
+    private static HikariDataSource dataSource;
+
+    private DataSourceConfig() {
+
+    }
+
+    public static HikariDataSource getInstance() {
+        if (dataSource == null) {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl("jdbc:mariadb://192.168.0.152:3306/tiggle");
+            config.setUsername("masterKDU");
+            config.setPassword("qwer1234");
+            config.setMaximumPoolSize(10);
+            config.setMinimumIdle(5);
+            dataSource = new HikariDataSource(config);
+        }
+        return dataSource;
+    }
+}
