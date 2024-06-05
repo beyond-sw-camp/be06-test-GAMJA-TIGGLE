e8a56dfb96f0d61f8c979f99f5d329eb45fb791e
postrel63 <postrel63@naver.com>
2024-06-05 14:48:36 +0900

    [Feat] ReservationServlet

--- src/main/java/org/example/tiggle/reservation/servlet/ReservationServlet.java
+++ src/main/java/org/example/tiggle/reservation/servlet/ReservationServlet.java
@@ -1,4 +1,67 @@
 package org.example.tiggle.reservation.servlet;
 
-public class ReservationServlet {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.Serializers;
+import org.example.tiggle.config.BaseResponse;
+import org.example.tiggle.config.BaseResponseMessage;
+import org.example.tiggle.reservation.DAO.ReservationDao;
+import org.example.tiggle.reservation.DTO.EmptySeatRequest;
+import org.example.tiggle.reservation.DTO.EmptySeatResponse;
+import org.example.tiggle.user.DAO.MemberDao;
+import org.example.tiggle.user.DTO.request.PostMemberReq;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.example.tiggle.config.BaseResponseMessage.TICKET_PURCHASE_FAIL_NOT_FOUND_SEAT;
+
+@WebServlet("/reservation/seat")
+public class ReservationServlet extends HttpServlet {
+    ReservationDao dao;
+    ObjectMapper mapper;
+
+    @Override
+    public void init() {
+        dao = new ReservationDao();
+        mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //잔여 좌석 조회
+
+
+        BufferedReader reader = req.getReader();
+        StringBuilder json = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            json.append(line);
+        }
+
+
+        EmptySeatRequest request = mapper.readValue(json.toString(), EmptySeatRequest.class);
+        ArrayList<EmptySeatResponse> responses = dao.emptySeat(request);
+
+        String jsonResponse;
+        if (!responses.isEmpty()) {
+            BaseResponse baseResponse = new BaseResponse(responses);
+            jsonResponse = mapper.writeValueAsString(baseResponse);
+        }else{
+            BaseResponse baseResponse = new BaseResponse(TICKET_PURCHASE_FAIL_NOT_FOUND_SEAT);
+            jsonResponse = mapper.writeValueAsString(baseResponse);
+        }
+
+
+        resp.getWriter().write(jsonResponse);
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+    }
 }
