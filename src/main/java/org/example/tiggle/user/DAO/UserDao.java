f7a077377a244c3eda284932e661cd96fd8fa078
yeon <dutls5367@gmail.com>
2024-06-05 16:23:14 +0900

    [Feat] UserDao Login기능 추가

--- src/main/java/org/example/tiggle/user/DAO/UserDao.java
+++ src/main/java/org/example/tiggle/user/DAO/UserDao.java
@@ -3,10 +3,13 @@ package org.example.tiggle.user.DAO;
 import com.zaxxer.hikari.HikariDataSource;
 
 import org.example.tiggle.config.DataSourceConfig;
+import org.example.tiggle.user.DTO.request.PostUserLoginReq;
+import org.example.tiggle.user.DTO.request.PostUserLoginRes;
 import org.example.tiggle.user.DTO.request.PostUserSignupReq;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
@@ -58,10 +61,50 @@ public class UserDao {
         return false;
     }
 
+    public PostUserLoginRes find(PostUserLoginReq dto) {
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            connection = dataSourceConfig.getConnection();
+            pstmt = connection.prepareStatement("SELECT * FROM user WHERE id=? AND password=?");
+            pstmt.setString(1, dto.getId());
+            pstmt.setString(2, dto.getPassword());
+            rs = pstmt.executeQuery();
+            PostUserLoginRes postUserLoginRes = null;
+            if (rs.next()) {
+                postUserLoginRes = new PostUserLoginRes(rs.getInt("userId"), rs.getString("id"), rs.getString("name"));
+            }
 
+            return postUserLoginRes;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                pstmt = null;
+            }
 
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                connection = null;
+            }
 
-
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                rs = null;
+            }
+        }
+    }
 }
 
 
